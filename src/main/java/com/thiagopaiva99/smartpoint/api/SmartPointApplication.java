45797309d645fe145900a8e58abd347c8ac833a1
Thiago Galvani <thiagopaiva99@gmail.com>
2019-03-03 23:35:22 -0300

    Adding initial files

---
+++ src/main/java/com/thiagopaiva99/smartpoint/api/SmartPointApplication.java
@@ -0,0 +1,13 @@
+package com.thiagopaiva99.smartpoint.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPointApplication.class, args);
+	}
+
+}
